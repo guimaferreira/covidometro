9539aa1c66d4b944a394593dc5391b68eb29ab8a
Guima Ferreira <guima@nucont.com>
2020-08-22 20:02:22 -0300

    Started Diagnosis Component

---
+++ components/Diagnosis/Diagnosis.jsx
@@ -0,0 +1,10 @@
+import Date from "../Date/Date";
+
+export default function Diagnosis({ diagnosis }) {
+    return (
+        <div>
+            <Date />
+            <h1>Situação Crítica</h1>
+        </div>
+    );
+}
