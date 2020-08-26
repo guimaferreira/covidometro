7c0bfb2c0116b0072f663acef5f0ae574a74ce38
Guima Ferreira <guima@nucont.com>
2020-08-26 12:58:08 -0300

    Intala Googla Analytics e SmartLook

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "../styles/globals.sass";
+import Router from "next/router";
+import withGA from "next-ga";
 
 function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
 
-export default App;
+export default withGA("G-NJVKQMWQ37", Router)(App);
--- pages/_document.js
+++ pages/_document.js
@@ -7,12 +7,27 @@ class MyDocument extends Document {
     }
 
     render() {
+        const smartLookScript =
+            process.env.NODE_ENV == "production"
+                ? `window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', 'f4d7549cf08a755115a103dcb0b5428b76bba634');`
+                : "";
+
         return (
             <Html>
                 <Head>
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: smartLookScript
+                        }}
                     />
                 </Head>
                 <body>
