3176e61a3a2f0442397bb8906b27fad766e2ebac
Guima Ferreira <guima@nucont.com>
2020-08-26 12:14:53 -0300

    Adiciona OpenGraph tags

--- components/Diagnosis/Diagnosis.jsx
+++ components/Diagnosis/Diagnosis.jsx
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import { format, formatDistanceStrict } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import Head from "next/head";
 import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import DiagnosisDate from "../DiagnosisDate/DiagnosisDate";
@@ -24,7 +25,7 @@ import DateLocal from "../../helpers/DateLocal.helper";
 
 const data = require("../../public/data/mg-claudio.json");
 
-export default function Diagnosis({}) {
+export default function Diagnosis({ city }) {
     const startIndex = _.findIndex(data, { deaths_cum: 1 }) - 1;
     const diagnosisData = data.slice(startIndex);
 
@@ -67,186 +68,190 @@ export default function Diagnosis({}) {
     const casesTime = getTime(current.cases_time),
         deathsTime = getTime(current.deaths_time);
 
+    const pageTitle = `COVIDOMETRO.com: ${city}`;
+    const pageImage = `/images/covidometro-${current.deaths_situation.toLowerCase()}.jpg`;
+
     return (
-        <div className={`${styles.container} ${styleColor}`}>
-            <h1>Mortalidade {current.deaths_situation}</h1>
-            <GaugeChart
-                id="gauge-chart1"
-                animDelay={100}
-                nrOfLevels={5}
-                marginInPercent={0.06}
-                arcPadding={0.02}
-                colors={colors}
-                arcWidth={0.4}
-                cornerRadius={4}
-                percent={current.deaths_gauge}
-                hideText={true}
-                needleColor={needleColor}
-                needleBaseColor={needleColor}
-            />
-            <div className={styles.text}>
-                <DiagnosisDate date={current.date} type={current.type} />
-                <DiagnosisDescription deltas={current.deaths_index_delta} />
-            </div>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Kpi type={current.type} value={casesTime}>
-                        1 caso a cada
-                    </Kpi>
-                </Grid>
-                <Grid item xs={6}>
-                    <Kpi type={current.type} value={deathsTime || "."}>
-                        {deathsTime ? "1 óbito a cada" : "Nenhum óbito"}
-                    </Kpi>
-                </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Kpi
-                        type={current.type}
-                        value={current.cases_cum}
-                        round={true}
-                    >
-                        Casos
-                    </Kpi>
-                </Grid>
-                <Grid item xs={6}>
-                    <Kpi
-                        type={current.type}
-                        value={current.deaths_cum}
-                        round={true}
-                    >
-                        Óbitos
-                    </Kpi>
-                </Grid>
-            </Grid>
-            <Card raised={true} className={styles.slider}>
-                <Slider
-                    classes={{
-                        valueLabel: styles.MuiSliderValueLabel,
-                        thumb: styles.MuiSliderThumb
-                    }}
-                    value={epoch}
-                    min={0}
-                    max={diagnosisData.length - 1}
-                    valueLabelDisplay="on"
-                    valueLabelFormat={sliderLabelFormat}
-                    onChange={(ev, value) => setEpoch(value)}
+        <>
+            <Head>
+                <title>{pageTitle}</title>
+                <meta
+                    name="og:description"
+                    content="Descubra qual a situação atual e o futuro da COVID-19 na sua cidade."
                 />
-            </Card>
-            <div>
-                <h2>Evolução dos Casos</h2>
-                <Typography>
-                    <div
-                        className={styles.chart}
-                        style={{ width: "100%", height: 200 }}
-                    >
-                        <ResponsiveContainer>
-                            <AreaChart
-                                data={chartData}
-                                margin={{
-                                    top: 0,
-                                    right: 0,
-                                    left: 0,
-                                    bottom: 0
-                                }}
-                            >
-                                <defs>
-                                    <linearGradient
-                                        id="colorUv"
-                                        x1="0"
-                                        y1="0"
-                                        x2="0"
-                                        y2="1"
-                                    >
-                                        <stop
-                                            offset="5%"
-                                            stopColor={needleColor}
-                                            stopOpacity={0.8}
-                                        />
-                                        <stop
-                                            offset="95%"
-                                            stopColor={needleColor}
-                                            stopOpacity={0}
-                                        />
-                                    </linearGradient>
-                                </defs>
-                                <XAxis
-                                    dataKey="date"
-                                    type="number"
-                                    domain={["dataMin", "dataMax"]}
-                                    tickFormatter={chartXTickFormat}
-                                />
-                                <YAxis
-                                    hide={true}
-                                    domain={["dataMin", "dataMax"]}
-                                />
-                                <Tooltip />
-                                <Legend
-                                    // payload={[
-                                    //     {
-                                    //         value: "Real",
-                                    //         type: "line"
-                                    //     },
-                                    //     {
-                                    //         value: "Previsão",
-                                    //         type: "line"
-                                    //     }
-                                    // ]}
-                                    verticalAlign="bottom"
-                                    height={24}
-                                    iconType="plainline"
-                                />
-                                <Area
-                                    data={chartData.filter(
-                                        (d) => d.type == "Real"
-                                    )}
-                                    type="monotone"
-                                    dataKey="cases_cum"
-                                    stroke={needleColor}
-                                    activeDot={{ r: 1 }}
-                                    fill="url(#colorUv)"
-                                    name="Real"
-                                />
-                                <Area
-                                    data={chartData.filter(
-                                        (d, key, arr) =>
-                                            d.type == "Previsão" ||
-                                            _.get(arr[key + 1], "type") ==
-                                                "Previsão"
-                                    )}
-                                    type="monotone"
-                                    dataKey="cases_cum"
-                                    stroke={needleColor}
-                                    activeDot={{ r: 1 }}
-                                    fill="url(#colorUv)"
-                                    strokeDasharray="4 4"
-                                    name="Previsão"
-                                />
-                            </AreaChart>
-                        </ResponsiveContainer>
-                    </div>
-                </Typography>
-                <Typography>
-                    <span className={styles.disclaimer}>
-                        <strong>Aviso Legal</strong>
-                        <br />
-                        <em>
-                            Esta é uma versão experimental não oficial do
-                            COVIDOMETRO.com, as informações desta versão podem
-                            sofrer alterações sem qualquer aviso prévio.
-                        </em>
-                        <br />
-                        Os dados reais foram extraídos dos boletins da
-                        Prefeitura no Instagram, as previsões estatísticas foram
-                        baseadas nestes dados.
-                        <br />
-                        Os dados de comparação com os óbitos de 2018 foram
-                        feitos de acordo com o relatório anual de mortalidade no
-                        Brasil (DataSUS, 2018).
-                    </span>
-                </Typography>
+                <meta property="og:image" content={pageImage} />
+            </Head>
+            <div className={`${styles.container} ${styleColor}`}>
+                <h1>Mortalidade {current.deaths_situation}</h1>
+                <GaugeChart
+                    id="gauge-chart1"
+                    animDelay={100}
+                    nrOfLevels={5}
+                    marginInPercent={0.06}
+                    arcPadding={0.02}
+                    colors={colors}
+                    arcWidth={0.4}
+                    cornerRadius={4}
+                    percent={current.deaths_gauge}
+                    hideText={true}
+                    needleColor={needleColor}
+                    needleBaseColor={needleColor}
+                />
+                <div className={styles.text}>
+                    <DiagnosisDate date={current.date} type={current.type} />
+                    <DiagnosisDescription deltas={current.deaths_index_delta} />
+                </div>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Kpi type={current.type} value={casesTime}>
+                            1 caso a cada
+                        </Kpi>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Kpi type={current.type} value={deathsTime || "."}>
+                            {deathsTime ? "1 óbito a cada" : "Nenhum óbito"}
+                        </Kpi>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Kpi
+                            type={current.type}
+                            value={current.cases_cum}
+                            round={true}
+                        >
+                            Casos
+                        </Kpi>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Kpi
+                            type={current.type}
+                            value={current.deaths_cum}
+                            round={true}
+                        >
+                            Óbitos
+                        </Kpi>
+                    </Grid>
+                </Grid>
+                <Card raised={true} className={styles.slider}>
+                    <Slider
+                        classes={{
+                            valueLabel: styles.MuiSliderValueLabel,
+                            thumb: styles.MuiSliderThumb
+                        }}
+                        value={epoch}
+                        min={0}
+                        max={diagnosisData.length - 1}
+                        valueLabelDisplay="on"
+                        valueLabelFormat={sliderLabelFormat}
+                        onChange={(ev, value) => setEpoch(value)}
+                    />
+                </Card>
+                <div>
+                    <h2>Evolução dos Casos</h2>
+                    <Typography>
+                        <div
+                            className={styles.chart}
+                            style={{ width: "100%", height: 200 }}
+                        >
+                            <ResponsiveContainer>
+                                <AreaChart
+                                    data={chartData}
+                                    margin={{
+                                        top: 0,
+                                        right: 0,
+                                        left: 0,
+                                        bottom: 0
+                                    }}
+                                >
+                                    <defs>
+                                        <linearGradient
+                                            id="colorUv"
+                                            x1="0"
+                                            y1="0"
+                                            x2="0"
+                                            y2="1"
+                                        >
+                                            <stop
+                                                offset="5%"
+                                                stopColor={needleColor}
+                                                stopOpacity={0.8}
+                                            />
+                                            <stop
+                                                offset="95%"
+                                                stopColor={needleColor}
+                                                stopOpacity={0}
+                                            />
+                                        </linearGradient>
+                                    </defs>
+                                    <XAxis
+                                        dataKey="date"
+                                        type="number"
+                                        domain={["dataMin", "dataMax"]}
+                                        tickFormatter={chartXTickFormat}
+                                    />
+                                    <YAxis
+                                        hide={true}
+                                        domain={["dataMin", "dataMax"]}
+                                    />
+                                    <Tooltip />
+                                    <Legend
+                                        verticalAlign="bottom"
+                                        height={24}
+                                        iconType="plainline"
+                                    />
+                                    <Area
+                                        data={chartData.filter(
+                                            (d) => d.type == "Real"
+                                        )}
+                                        type="monotone"
+                                        dataKey="cases_cum"
+                                        stroke={needleColor}
+                                        activeDot={{ r: 1 }}
+                                        fill="url(#colorUv)"
+                                        name="Real"
+                                    />
+                                    <Area
+                                        data={chartData.filter(
+                                            (d, key, arr) =>
+                                                d.type == "Previsão" ||
+                                                _.get(arr[key + 1], "type") ==
+                                                    "Previsão"
+                                        )}
+                                        type="monotone"
+                                        dataKey="cases_cum"
+                                        stroke={needleColor}
+                                        activeDot={{ r: 1 }}
+                                        fill="url(#colorUv)"
+                                        strokeDasharray="4 4"
+                                        name="Previsão"
+                                    />
+                                </AreaChart>
+                            </ResponsiveContainer>
+                        </div>
+                    </Typography>
+                    <Typography>
+                        <span className={styles.disclaimer}>
+                            <strong>Aviso Legal</strong>
+                            <br />
+                            <em>
+                                Esta é uma versão experimental não oficial do
+                                COVIDOMETRO.com, as informações desta versão
+                                podem sofrer alterações sem qualquer aviso
+                                prévio.
+                            </em>
+                            <br />
+                            Os dados reais foram extraídos dos boletins da
+                            Prefeitura no Instagram, as previsões estatísticas
+                            foram baseadas nestes dados.
+                            <br />
+                            Os dados de comparação com os óbitos de 2018 foram
+                            feitos de acordo com o relatório anual de
+                            mortalidade no Brasil (DataSUS, 2018).
+                        </span>
+                    </Typography>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
